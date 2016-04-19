0459b3cc3afed280b3afe32b8b28e6f6f5af0e76
Dany Paredes <danywalls4@gmail.com>
2016-04-19 23:40:46 +0200

    component de peppa pig
    ejemplo de creación de component con angular2

---
+++ src/app/components/peppapig.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector:'peppapig-component',
+    template:'<h1> Soy La peppa pig</h1>'
+})
+export class PeppaPigComponent{
+    
+}
---
+++ src/app/seed-app.ts
@@ -0,0 +1,25 @@
+import {Component} from 'angular2/core';
+import {Router, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {Home} from './components/home/home';
+import {About} from './components/about/about';
+import {RepoBrowser} from './components/repo-browser/repo-browser';
+import {PeppaPigComponent} from './components/peppapig.component'
+
+@Component({
+  selector: 'seed-app',
+  providers: [],
+  pipes: [],
+  directives: [ROUTER_DIRECTIVES,PeppaPigComponent],
+  templateUrl: 'app/seed-app.html',
+})
+@RouteConfig([
+  { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
+  { path: '/about',      component: About,       name: 'About' },
+  { path: '/github/...', component: RepoBrowser, name: 'RepoBrowser' },
+])
+export class SeedApp {
+
+  constructor() {}
+
+}
