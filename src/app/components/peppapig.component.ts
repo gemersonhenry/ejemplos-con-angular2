69123192f2d4cb377cabc284f47c8ad6c3e10e0f
Dany Paredes <danywalls4@gmail.com>
2016-05-03 22:44:50 +0200

    images and materializecss

--- src/app/components/peppapig.component.ts
+++ src/app/components/peppapig.component.ts
@@ -2,24 +2,28 @@ import {Component} from 'angular2/core';
 import {PeppaPigService} from '../services/peppapig.services';
 
 @Component({
-    selector:'peppapig-component',
-    template:`
+    selector: 'peppapig-component',
+    template: `
               <div class="row">
+              
               <div class="col s12">
                 <h2>{{welcome}}</h2>
-                <h2 [textContent]="welcome"></h2>
-                <h2 bind-textContent="welcome"></h2>
+              <a class="waves-effect waves-light btn" [class.disabled] = "isdisabled" 
+              
+             >button</a>
                 </div>
               </div>`,
     providers: [PeppaPigService]
 })
-export class PeppaPigComponent{
-    
+export class PeppaPigComponent {
+
     friends: string[];
-    welcome: string = "Peppa Pig Friends"
-    
-    constructor(private _peppapigService: PeppaPigService){
-       
+    isdisabled: Boolean = false;
+    bigicon: Boolean = true;
+    welcome: string = "interpolation"
+
+    constructor(private _peppapigService: PeppaPigService) {
+
         this.friends = this._peppapigService.getPeppaFriends();
         console.log(this.friends);
     }
