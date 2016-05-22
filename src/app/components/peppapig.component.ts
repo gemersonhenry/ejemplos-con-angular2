4b85bbf6f473670ba5463a8d019d666955f93fb3
Dany Paredes <danywalls4@gmail.com>
2016-05-22 22:56:41 +0200

    add style and class
    added class and style

--- src/app/components/peppapig.component.ts
+++ src/app/components/peppapig.component.ts
@@ -6,7 +6,7 @@ import {PeppaPigService} from '../services/peppapig.services';
     template: `
               <div class="row">
               
-              <div class="col s12">
+              <div class="col s12" [style.backgroundColor] = "isdisabled ? 'gray' : 'blue'">
                 <h2>{{welcome}}</h2>
                 <input type="text" >
               <a class="waves-effect waves-light btn" [class.disabled] = "isdisabled" >
@@ -20,7 +20,7 @@ import {PeppaPigService} from '../services/peppapig.services';
 export class PeppaPigComponent {
 
     friends: string[];
-    isdisabled: Boolean = false;
+    isdisabled: Boolean = true;
     bigicon: Boolean = true;
     welcome: string = "Angular2 y Peppa Pig"
     age: string
