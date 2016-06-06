1072ed117fe99aeed29e62828455c7bcefea8796
Dany Paredes <danywalls4@gmail.com>
2016-06-06 17:00:47 +0200

    Event Binding

--- src/app/components/peppapig.component.ts
+++ src/app/components/peppapig.component.ts
@@ -2,32 +2,44 @@ import {Component} from '@angular/core';
 import {PeppaPigService} from '../services/peppapig.services';
 
 @Component({
-    selector: 'peppapig-component',
-    template: `
+  selector: 'peppapig-component',
+  template: `
               <div class="row">
-
-              <div class="col s12" [style.backgroundColor] = "isdisabled ? 'gray' : 'blue'">
+              <div  class="col s12" [style.backgroundColor] = "isdisabled ? 'gray' : 'blue'">
                 <h2>{{welcome}}</h2>
                 <input type="text" >
-              <a class="waves-effect waves-light btn" [class.disabled] = "isdisabled" >
-             class binding </a>
-             <a class="waves-effect waves-light btn">style binding</a>
+              <a (mouseover) = "CleanFriends()"  class="waves-effect waves-light btn" [class.disabled] = "isdisabled" >
+                  Clean Friends</a>
+             <a (click)="GetFriends()" class="waves-effect waves-light btn">Get Friends</a>
                 </div>
+                  <ul class="collection" >
+                      <li *ngFor="let f of friends">
+                        <a  class="collection-item">{{f}}</a> 
+                      </li>
+                  </ul>
+
               </div>`,
-    providers: [PeppaPigService]
+  providers: [PeppaPigService]
 })
 export class PeppaPigComponent {
-    friends: string[];
-    isdisabled: Boolean = true;
-    bigicon: Boolean = true;
-    welcome: string = "Angular2 y Peppa Pig"
-    age: string = "22"
-    address: string = "ave catluyna";
+  friends: string[];
+  isdisabled: Boolean = true;
+  bigicon: Boolean = true;
+  welcome: string = "Angular2 y Peppa Pig";
+  age: string = "22";
+  address: string = "ave catluyna";
 
-    constructor(private _peppapigService: PeppaPigService) {
+  constructor(private _peppapigService: PeppaPigService) {
+      
+  }
 
-        this.friends = this._peppapigService.getPeppaFriends();
-        console.log(this.friends);
-      console.log("heellooo");
-    }
+  CleanFriends(event: any) {
+    console.log(event);
+    this.friends = [];
+  }
+  //metodo que se llamara al realizar click en get friend  
+  GetFriends() {
+    this.friends = this._peppapigService.getPeppaFriends();
+    console.log(this.friends);
+  }
 }
--- src/app/seed-app.ts
+++ src/app/seed-app.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {Router, RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {Home} from './components/home/home';
 import {About} from './components/about/about';
-import {PeppaPigComponent} from './components/peppapig.component'
+import {PeppaPigComponent} from './components/peppapig.component';
 
 @Component({
   selector: 'seed-app',
@@ -17,6 +17,10 @@ import {PeppaPigComponent} from './components/peppapig.component'
 ])
 export class SeedApp {
 
-  constructor() {}
+    constructor() {
+    
+  }
+
+
 
 }
